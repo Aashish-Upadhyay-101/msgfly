d6d876806e11f4b8b3e77cce0cdd51319e0ea576
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2022-10-26 00:19:02 +0545

    feat: basic channel setup

--- msgfly/asgi.py
+++ msgfly/asgi.py
@@ -10,14 +10,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter 
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from chats import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msgfly.settings')
 
+django_asgi_application = get_asgi_application()
+
 # application = get_asgi_application()
 application = ProtocolTypeRouter(
    {
-     "http": get_asgi_application (),
+     "http": django_asgi_application,
+     "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+     )
    }
 )
