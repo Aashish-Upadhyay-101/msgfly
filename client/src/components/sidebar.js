4cc4d2638dbd9bd6e1e707083ac2247ba4098782
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2022-11-08 01:05:22 +0545

    fix: add small emoji in sidebar

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -5,87 +5,87 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="user-inbox">
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
         <div className="chat-head">
-          <h4>(ˆ_ˆ) Aashish Upadhyay</h4>
+          <h4>💠 Aashish Upadhyay</h4>
           <i>This is dummy message</i>
         </div>
       </div>
